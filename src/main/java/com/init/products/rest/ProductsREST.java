2c9b2d64f0ba7d131dee42fb4781f9f495c12804
andresmiqueas <andresmiqueas27@gmail.com>
2022-09-06 22:51:58 -0300

    Conexion a la base de datos, se agregaron productos, name + id

--- src/main/java/com/init/products/rest/ProductsREST.java
+++ src/main/java/com/init/products/rest/ProductsREST.java
@@ -1,14 +1,19 @@
 package com.init.products.rest;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.init.products.dao.ProductsDAO;
 import com.init.products.entitys.Product;
 
 
@@ -18,14 +23,34 @@ public class ProductsREST {
 
 	
 	
+	@Autowired
+	private ProductsDAO productDAO;
+	
+	
 	@GetMapping
 	public ResponseEntity<List<Product>> getProduct(){
-		
-		return null;
-		//return ResponseEntity.ok();
+		List<Product> products = productDAO.findAll(); //Conexion a la base datos, consulto registros, convirtio registros en objeto y lo regreso como una lista
+		return ResponseEntity.ok(products);
 		
 	}
 	
+	
+	@RequestMapping(value= "{productId}") // /products/{productId} -> /products/1
+	public ResponseEntity<Product> getProductById(@PathVariable("productId") Long productId){
+		Optional<Product> optionalProduct = productDAO.findById(productId);
+		if (optionalProduct.isPresent()) {
+			return ResponseEntity.ok(optionalProduct.get());
+		}else {
+			return ResponseEntity.noContent().build();
+	}
+	}
+
+	@PostMapping
+	public ResponseEntity <Product> createProduct(@RequestBody Product product){
+		Product newProduct = productDAO.save(product);
+		return ResponseEntity.ok(newProduct);
+		
+	}
 	
 	//@GetMapping
 	//@RequestMapping(value="hello", method=RequestMethod.GET)
