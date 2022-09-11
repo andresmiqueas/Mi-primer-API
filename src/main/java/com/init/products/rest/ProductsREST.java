ea984c633eae1302a9faebb8116eddc106b323fb
andresmiqueas <andresmiqueas27@gmail.com>
2022-09-11 10:06:31 -0300

    Ultimo push, aplicacion ya funcionando

--- src/main/java/com/init/products/rest/ProductsREST.java
+++ src/main/java/com/init/products/rest/ProductsREST.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.init.products.dao.ProductsDAO;
@@ -45,12 +47,34 @@ public class ProductsREST {
 	}
 	}
 
-	@PostMapping
+	@PostMapping // /products (POST)
 	public ResponseEntity <Product> createProduct(@RequestBody Product product){
 		Product newProduct = productDAO.save(product);
 		return ResponseEntity.ok(newProduct);
 		
 	}
+	
+	@DeleteMapping(value="{productId}") // /products (DELETE)
+	public ResponseEntity <Void> deleteProduct(@PathVariable("productId") Long productId){
+		productDAO.deleteById(productId);
+		return ResponseEntity.ok(null);
+		
+	}
+	
+	@PutMapping
+	public ResponseEntity <Product> updateProduct(@RequestBody Product product){
+		Optional<Product> optionalProduct = productDAO.findById(product.getId());
+		if (optionalProduct.isPresent()) {
+			Product updateProduct = optionalProduct.get();
+			updateProduct.setName(product.getName());
+			productDAO.save(updateProduct);
+			return ResponseEntity.ok(updateProduct);
+		}else {
+			return ResponseEntity.notFound().build();
+	}
+	}
+	
+	
 	
 	//@GetMapping
 	//@RequestMapping(value="hello", method=RequestMethod.GET)
