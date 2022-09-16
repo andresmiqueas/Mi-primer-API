ce6875cc04212e5db326e96c768244d97e50abb6
unknown <jpaz@xytech.com.ar>
2022-09-15 21:37:41 -0300

    completo productos y creo query para db

--- src/main/java/com/init/products/entitys/Product.java
+++ src/main/java/com/init/products/entitys/Product.java
@@ -18,21 +18,86 @@ public class Product {
 	private Long id;
 	
 	
-	@Column(name="name", nullable=false, length=30)
+	@Column(name="name", nullable=false, length=300)
 	private String name;
+	
+	@Column(name="img_url", nullable=false, length=300)
+	private String img_url;
+	
+	@Column(name="description", nullable=false, length=600)
+	private String description;
+	
+	@Column(name="price", nullable=false)
+	private Long price;
+	
+	@Column(name="stock", nullable=false)
+	private Long stock;
+
+	@Column(name="category", nullable=false, length=100)
+	private String category;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getImg_url() {
+		return img_url;
+	}
+
+	public void setImg_url(String img_url) {
+		this.img_url = img_url;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getPrice() {
+		return price;
+	}
+
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+
+	public Long getStock() {
+		return stock;
+	}
+
+	public void setStock(Long stock) {
+		this.stock = stock;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
 	
+	
+	
+	
+
 	
 	
 }
